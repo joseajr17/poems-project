11c29235e0194bcbbd9b743dff7117baceb49cf1
José Antonio <josejr017@gmail.com>
2025-03-26 15:35:54 -0300

    create stanza

---
+++ backend/src/main/java/com/poems/backend/controller/StanzaController.java
@@ -0,0 +1,26 @@
+package com.poems.backend.controller;
+
+import com.poems.backend.domain.stanza.Stanza;
+import com.poems.backend.domain.stanza.StanzaRequestDTO;
+import com.poems.backend.service.StanzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/stanza")
+public class StanzaController {
+
+    @Autowired
+    private StanzaService stanzaService;
+
+    @PostMapping("/poem/{poemId}")
+    public ResponseEntity<Stanza> addStanzasToPoem(@PathVariable UUID poemId, @RequestBody StanzaRequestDTO data) {
+        Stanza stanzas = stanzaService.addStanzaToEvent(poemId, data);
+        return ResponseEntity.ok(stanzas);
+    }
+
+
+}
