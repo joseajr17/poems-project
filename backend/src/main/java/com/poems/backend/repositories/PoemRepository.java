4a9f630c5224e82468c341e80d7b02e64138b6d9
José Antonio <josejr017@gmail.com>
2025-03-27 22:04:32 -0300

    fix: deleted files

--- backend/src/main/java/com/poems/backend/repositories/PoemRepository.java
+++ backend/src/main/java/com/poems/backend/repositories/PoemRepository.java
@@ -1,8 +1,13 @@
 package com.poems.backend.repositories;
 
 import com.poems.backend.domain.poem.Poem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.UUID;
 
 public interface PoemRepository extends JpaRepository<Poem, UUID> {
