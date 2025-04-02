c92a26df9eedbded35b9169705f5967f6c46a371
José Antonio <josejr017@gmail.com>
2025-04-01 22:18:25 -0300

    PoemForm component v1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import { PoemList } from "./components/PoemList"
+import { Home } from "./components/Home";
 
 function App() {
-
   return (
     <div className="flex flex-col items-center bg-gray-800 font-mono text-white min-h-screen min-w-screen">
-      <PoemList />
+      <Home />
     </div>
   )
 }
 
 export default App
+ 
--- frontend/src/components/PoemList/index.tsx
+++ frontend/src/components/PoemList/index.tsx
@@ -30,37 +30,36 @@ export function PoemList() {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-center items-center m-2 p-2 border w-3/4">
             {poems.map(poem => (
-                <div key={poem.id} className='transition-transform transform hover:scale-101 hover:shadow-lg w-full'>
-                    <Card className="flex flex-col min-w-full ">
-                        <CardHeader>
-                            <CardTitle >{poem.title}</CardTitle>
-                            <CardDescription>Poema escrito por: {poem.author}</CardDescription>
-                        </CardHeader>
-                        <CardContent className="flex-grow self-start">
-                            <pre>{poem.content.split("\n").slice(0, 4).join("\n") + (poem.content.split("\n").length > 4 ? "..." : "")}</pre>
+                <Card key={poem.id} className="flex flex-col min-w-full transition-transform transform hover:scale-101 hover:shadow-lg w-full h-[400px]">
+                    <CardHeader>
+                        <CardTitle >{poem.title}</CardTitle>
+                        <CardDescription>Poema escrito por: {poem.author}</CardDescription>
+                    </CardHeader>
+                    <CardContent className="flex-grow self-start">
+                        <pre>{poem.content.split("\n").slice(0, 6).join("\n") + (poem.content.split("\n").length > 6 ? "..." : "")}</pre>
 
-                            <div className="flex justify-end">
-                                <button
-                                    className="text-blue-500 hover:underline"
-                                    onClick={() => setSelectedPoemId(poem.id)}
-                                >
-                                    Ver mais
-                                </button>
+                        <div className="flex justify-end">
+                            <button
+                                className="text-blue-500 hover:underline"
+                                onClick={() => setSelectedPoemId(poem.id)}
+                            >
+                                Ver mais
+                            </button>
 
-                                {selectedPoemId !== null && (
-                                    <PoemModal
-                                        poemId={selectedPoemId}
-                                        isModalOpen={selectedPoemId !== null}
-                                        setIsModalOpen={() => setSelectedPoemId(null)}
-                                    />
-                                )}
-                            </div>
-                        </CardContent>
-                        <CardFooter className="mt-auto">
-                            Poema escrito em: {new Date(poem.date).toLocaleDateString()}
-                        </CardFooter>
-                    </Card>
-                </div>
+                            {selectedPoemId !== null && (
+                                <PoemModal
+                                    poemId={selectedPoemId}
+                                    isModalOpen={selectedPoemId !== null}
+                                    setIsModalOpen={() => setSelectedPoemId(null)}
+                                />
+                            )}
+                        </div>
+                    </CardContent>
+                    <CardFooter className="mt-auto">
+                        Poema escrito em: {new Date(poem.date).toLocaleDateString()}
+                    </CardFooter>
+                </Card>
+
             ))}
         </div>
     );
