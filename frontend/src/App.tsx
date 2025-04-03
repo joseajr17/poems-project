a809458a84c309bad7af9695cc5a9a52056dae46
José Antonio <josejr017@gmail.com>
2025-04-03 18:47:49 -0300

    About component v1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Home } from "./components/Home";
 
 function App() {
   return (
-    <div className="flex flex-col items-center bg-white font-mono text-gray min-h-screen w-screen">
+    <div className="flex flex-col items-center bg-white font-mono text-gray min-h-screen w-full ">
       <Header />
       <Home />
     </div>
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -2,15 +2,15 @@ import { IoBook } from "react-icons/io5";
 
 export function Header() {
     return (
-        <header className="bg-gray-800 text-white px-10 py-8 relative flex justify-between items-center w-full">
+        <header className="bg-gray-800 text-white px-10 py-10 relative flex justify-between items-center w-full">
 
-            <a href="/" className="flex gap-2 items-center text-xl font-bold hover:text-sky-500">
+            <a href="/" className="flex gap-2 items-center text-2xl font-bold hover:text-sky-500">
                 <IoBook />Acervo do Poeta Severino C. de Albuquerque
             </a>
             <nav className="flex gap-10">
-                <a href="/#about" className="hover:text-sky-500 hover:underline">Sobre</a>
-                <a href="/#photos" className="hover:text-sky-500 hover:underline">Fotos</a>
-                <a href="/#poemList" className="hover:text-sky-500 hover:underline">Poemas</a>
+                <a href="#about" className="hover:text-sky-500 hover:underline">Sobre</a>
+                <a href="#photos" className="hover:text-sky-500 hover:underline">Fotos</a>
+                <a href="#poemList" className="hover:text-sky-500 hover:underline">Poemas</a>
                 <a href="/admin" className="hover:text-sky-500 hover:underline">Admin</a>
             </nav>
         </header>
