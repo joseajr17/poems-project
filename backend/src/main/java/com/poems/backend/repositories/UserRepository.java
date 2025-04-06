3d01cf016ff6bab8b882803bd71c7f041a95d90e
José Antonio <josejr017@gmail.com>
2025-04-05 21:44:33 -0300

    creating userEntity and userRepository

---
+++ backend/src/main/java/com/poems/backend/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.poems.backend.repositories;
+
+import com.poems.backend.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByLogin(String login);
+}
