991566dbabddaa4a45e714dd3bf523d5728ed03d
José Antonio <josejr017@gmail.com>
2025-05-09 18:46:09 -0300

    Deploy no Render

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: process.env.VITE_BASE_PATH || "/poems-project",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
